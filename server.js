6ba6c7ed746e90eadf6faff97e5ec02be9a01037
Alexander Byun <alexanderbyun@MacBook-Pro.attlocal.net>
2022-05-19 01:16:30 -0400

    MVP

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// -------------------------------------------------
+// ---------------------------------
 // Dependencies
-// -------------------------------------------------
+// ---------------------------------
 const express = require('express');
 const methodOverride  = require('method-override');
 const mongoose = require ('mongoose');
@@ -11,18 +11,20 @@ const db = mongoose.connection;
 require('dotenv').config()
 
 
-// -------------------------------------------------
+// ---------------------------------
 // Port
-// -------------------------------------------------
+// ---------------------------------
 // Allow use of Heroku's port or your own local port, depending on the environment
 const PORT = process.env.PORT || 3003;
 
 
-// -------------------------------------------------
+// ---------------------------------
 // Database
-// -------------------------------------------------
+// ---------------------------------
 // How to connect to the database either via heroku or locally
 const MONGODB_URI = process.env.MONGODB_URI;
+// const mongoURI = "mongodb://localhost:27017/desks"
+
 
 // Connect to Mongo &
 // Fix Depreciation Warnings from Mongoose
@@ -31,15 +33,19 @@ mongoose.connect(MONGODB_URI, () => {
   console.log('Mongo live')
 });
 
+// mongoose.connect(mongoURI, () => {
+//   console.log('Mongo live')
+// });
+
 // Error / success
 // db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 // db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 // db.on('disconnected', () => console.log('mongo disconnected'));
 
 
-// -------------------------------------------------
+// ---------------------------------
 // Middleware
-// -------------------------------------------------
+// ---------------------------------
 
 //use public folder for static assets
 app.use(express.static('public'));
@@ -52,116 +58,115 @@ app.use(express.json());// returns middleware that only parses JSON - may or may
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
 
-// -------------------------------------------------
+// ---------------------------------
 // Models
-// -------------------------------------------------
+// ---------------------------------
 const Desks = require("./models/schema.js");
 const desksSeed = require("./models/seed.js");
 
 
-// -------------------------------------------------
+// ---------------------------------
 // Routes
-// -------------------------------------------------
-
-// NEW View
-// app.get("/new", (req, res) => {
-//   res.render("new.ejs");
-// });
+// ---------------------------------
 
 // Edit
-// GET /:id/edit
-// app.get("/:id/edit", (req, res) => {
-//   Desks.findById(req.params.id, (err, foundDesks) => {
-//     res.render("edit.ejs", {
-//       desks: foundDesks,
-//     });
-//   });
-// });
+app.get("/:id/edit", (req, res) => {
+  Desks.findById(req.params.id, (err, foundDesks) => {
+    res.render("edit.ejs", {
+      desks: foundDesks,
+    });
+  });
+});
 
 // Update
-// PUT /:id
-// app.put("/:id", (req, res) => {
-//   Desks.findByIdAndUpdate(
-//     req.params.id,
-//     req.body,
-//     { new: true },
-//     (err, updatedDesks) => {
-//       if (err) {
-//         console.log(err.message);
-//       } else {
-//         res.redirect("/desks");
-//       }
-//     }
-//   );
-// });
+app.put("/:id", (req, res) => {
+  Desks.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (err, updatedDesks) => {
+      if (err) {
+        console.log(err.message);
+      } else {
+        res.redirect(req.params.id);
+      }
+    }
+  );
+});
 
-// DELETE desks/:id
-// app.delete("/:id", (req, res) => {
-//   Desks.findByIdAndRemove(req.params.id, (err, data) => {
-//     if (err) {
-//       console.log(err.message);
-//     } else {
-//       res.redirect("/desks");
-//     }
-//   });
-// });
+// Delete
+app.delete("/:id", (req, res) => {
+  Desks.findByIdAndRemove(req.params.id, (err, data) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      res.redirect("/gallery");
+    }
+  });
+});
 
-// Show
-// GET desks/:id
-// app.get("/:id", (req, res) => {
-//   Desks.findById(req.params.id, (err, foundDesks) => {
-//     res.render("show.ejs", {
-//       desks: foundDesk,
-//     });
-//   });
-// });
+// NEW
+app.get("/new", (req, res) => {
+  res.render("new.ejs");
+});
 
-// GET
-//Delete below after seed complete
+// Index
 app.get("/", (req, res) => {
   res.render("index.ejs"
   );
 });
 
-// app.get("/", (req, res) => {
-//   Desks.find({}, (err, allDesks) => {
-//     res.render("index.ejs", {
-//       desks: allDesks,
-//     });
-//   });
-// });
-
-// Create
-// POST new desk
-// app.post("/", (req, res) => {
-//   Desks.create(req.body, (err, createdPark) => {
-//     res.redirect("/desks");
-//   });
-// });
-
-// Seed data add webpage
-app.get('/seed', (req, res)=>{
-  Desks.create(desksSeed,
-      (err, desksSeed)=>{
-        res.redirect('/');
-      }
-  )
-})
-
-// -------------------------------------------------
-// Listener
-// -------------------------------------------------
-app.listen(PORT, () => console.log( 'Listening on port:', PORT));
-
-// -------------------------------------------------
-// Seed data add
-// -------------------------------------------------
-Desks.create(desksSeed, (err, data) => {
-  if (err) console.log(err.message);
-  console.log("Added provided desks data....");
+// Gallery
+app.get("/gallery", (req, res) => {
+  Desks.find({}, (err, allDesks) => {
+    res.render("gallery.ejs", {
+      desks: allDesks,
+    });
+  });
 });
 
-// -------------------------------------------------
+// Show
+app.get('/:id', (req, res) => {
+  Desks.findById(req.params.id,(error,foundDesk) => {
+    res.render('show.ejs',{
+      desks: foundDesk,
+    })
+  })
+})
+
+// Create
+app.post("/", (req, res) => {
+  Desks.create(req.body, (err, createdDesks) => {
+    res.redirect("/gallery");
+  });
+});
+
+// Seed data add webpage
+// app.get('/seed', (req, res)=>{
+//   Desks.create(desksSeed,
+//       (err, desksSeed)=>{
+//         res.redirect('/');
+//       }
+//   )
+// })
+
+
+// ---------------------------------
+// Listener
+// ---------------------------------
+app.listen(PORT, () => console.log( 'Listening on port:', PORT));
+
+
+// ---------------------------------
+// Seed data add
+// ---------------------------------
+// Desks.create(desksSeed, (err, data) => {
+//   if (err) console.log(err.message);
+//   console.log("Added provided desks data");
+// });
+
+
+// ---------------------------------
 // Drop collection
-// -------------------------------------------------
+// ---------------------------------
 // Desks.collection.drop();
